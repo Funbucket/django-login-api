0750276ebc4147662a80aa9fb9201dd9ec0d7175
Joe <outofchng@gmail.com>
2022-02-21 16:03:34 +0900

    astar Added

--- server_dev/server_dev/settings.py
+++ server_dev/server_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xpvyv#=n^8x)$^%a!qjlxa#yvciwh+yy&dini(sgi5rc*4l4v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
