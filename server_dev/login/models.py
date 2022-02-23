c13bfd674c5a444af51a159355ab968c228ecadf
Joe <outofchng@gmail.com>
2022-02-23 18:49:24 +0900

    serializer Applied

--- server_dev/login/models.py
+++ server_dev/login/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class LoginUser(models.Model):
   user_id = models.CharField(max_length=20, unique=True, null=False, default=False)
   user_pw = models.CharField(max_length=256, null=False, default=False)
--- server_dev/login/views.py
+++ server_dev/login/views.py
@@ -1,70 +1,41 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LoginUser
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import check_password
+from .serialize import LoginUserSerializer
 
 # Create your views here.
 
 
 class AppLogin(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-
-        user = LoginUser.objects.filter(user_id=user_id).first()
+        serializer = LoginUserSerializer(request.data)
+        user = LoginUser.objects.filter(user_id=serializer.data["user_id"]).first()
+        user_pw = request.data.get('user_pw', "")
 
         if user is None:
             return Response(dict(msg="해당 사용자가 없습니다."))
 
         if check_password(user_pw, user.user_pw):
-            return Response(dict(
-                msg="로그인 성공",
-                user_id=user.user_id,
-                user_pw=user.user_pw_crypted,
-                birth_day=user.birth_day,
-                gender=user.gender,
-                email=user.email,
-                name=user.name,
-                age=user.age
-            ))
+            return Response(dict(msg="로그인 성공"))
         else:
             return Response(dict(msg="비밀번호가 틀립니다."))
 
 
 class RegistUser(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-        birth_day = request.data.get('birth_day', None)
-        gender = request.data.get('gender', "male")
-        email = request.data.get('email', "")
-        name = request.data.get('name', "")
-        age = request.data.get('age', 20)
-        user_pw_crypted = make_password(user_pw)
+        serializer = LoginUserSerializer(request.data)
 
-        user = LoginUser.objects.filter(user_id=user_id).first()
+        # 아이디 중복 확인
+        if LoginUser.objects.filter(user_id=serializer.data['user_id']).exists():
+            user = LoginUser.objects.filter(user_id=serializer.data['user_id']).first()
+            data = dict(
+                msg='이미 존재하는 아이디입니다.',
+                user_id=user.user_id,
+                user_pw=user.user_pw
+            )
+            return Response(data)
 
-        if user is not None:
-            return Response(dict(msg='동일한 아이디가 있습니다.'))
+        user = serializer.create(request.data)
 
-        LoginUser.objects.create(
-            user_id=user_id,
-            user_pw=user_pw_crypted,
-            birth_day=birth_day,
-            gender=gender,
-            email=email,
-            name=name,
-            age=age
-        )
-
-        data = dict(
-            user_id=user_id,
-            user_pw=user_pw_crypted,
-            birth_day=birth_day,
-            gender=gender,
-            email=email,
-            name=name,
-            age=age
-        )
-
-        return Response(data)
+        return Response(LoginUserSerializer(user).data)
